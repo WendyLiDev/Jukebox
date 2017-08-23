4561ccf692370e01ee213c817d974a565bb66461
Wendy Li <wli025@ucr.edu>
2017-08-22 18:49:02 -0700

    Initial Commit

---
+++ Jukebox/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Jukebox
+//
+//  Created by Wendy Li on 8/22/17.
+//  Copyright (c) 2017 ___wendy_li___. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
